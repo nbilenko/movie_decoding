c62536147d986bfff1e3d49eca340eb361fcdf38
nbilenko <nbilenko@berkeley.edu>
2014-12-15 20:25:00 -0800

    option for loading a list of indices

--- code/loadData.m
+++ code/loadData.m
@@ -1,16 +1,23 @@
-function data = loadData(opts)
+function [data, opts] = loadData(opts)
     if opts.firstlast
         nframes = 2;
     else
         nframes = opts.nframes;
     end
+    if any(strcmp('tp_list', fieldnames(opts))) & ~isempty(opts.tp_list)
+        opts.nT = length(opts.tp_list);
+        opts.firstclip = opts.tp_list(1);
+    else
+        opts.tp_list = opts.firstclip:opts.firstclip-1+opts.nT;
+    end
+
 
     data.guesses = zeros(opts.nT, opts.nG, nframes, opts.imsize(1), opts.imsize(2), opts.imsize(3));
     data.ocs = zeros(opts.nT, nframes, opts.imsize(1), opts.imsize(2), opts.imsize(3));
     data.llh = zeros(opts.nT, opts.nG);
 
-    for i=[opts.firstclip:opts.firstclip-1+opts.nT]
-        code = sprintf('data%03d',i);
+    for i=1:opts.nT
+        code = sprintf('data%03d',opts.tp_list(i));
         fname = strcat(code,'.hf5');
         floc = strcat(opts.dataFolder,fname);
         disp(floc);
@@ -21,16 +28,16 @@ function data = loadData(opts)
         clipz = permute(clipz, [4 3 2 1]);
         llh = h5read(floc, '/llh');
         llh = llh(1:opts.nG, 1);
-        data.llh(i-opts.firstclip+1, :) = (llh-min(llh))/(max(llh) - min(llh));
+        data.llh(i, :) = (llh-min(llh))/(max(llh) - min(llh));
 
         if opts.firstlast
-            data.guesses(i-opts.firstclip+1, :, 1, :, :, :) = squeeze(guessez(1:opts.nG, 1, :, :, :));
-            data.guesses(i-opts.firstclip+1, :, 2, :, :, :) = squeeze(guessez(1:opts.nG, opts.nframes, :, :, :));
-            data.ocs(i-opts.firstclip+1, 1, :, :, :) = squeeze(clipz(1, :, :, :));
-            data.ocs(i-opts.firstclip+1, 2, :, :, :) = squeeze(clipz(opts.nframes, :, :, :));
+            data.guesses(i, :, 1, :, :, :) = squeeze(guessez(1:opts.nG, 1, :, :, :));
+            data.guesses(i, :, 2, :, :, :) = squeeze(guessez(1:opts.nG, opts.nframes, :, :, :));
+            data.ocs(i, 1, :, :, :) = squeeze(clipz(1, :, :, :));
+            data.ocs(i, 2, :, :, :) = squeeze(clipz(opts.nframes, :, :, :));
         else
-            data.guesses(i-opts.firstclip+1, :, :, :, :, :) = guessez(1:opts.nG,:,:,:,:);
-            data.ocs(i-opts.firstclip+1, :, :, :, :) = clipz;
+            data.guesses(i, :, :, :, :, :) = guessez(1:opts.nG,:,:,:,:);
+            data.ocs(i, :, :, :, :) = clipz;
         end
     end
 end
